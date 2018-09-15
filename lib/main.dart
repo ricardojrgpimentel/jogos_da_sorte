114a941c24a782a42b89a1bd30c9ad09680174c2
Ricardo Pimentel <ricardojrgpimentel@gmail.com>
2018-09-15 17:34:02 +0100

    moved main Widget out of MaterialApp to build routes

--- lib/main.dart
+++ lib/main.dart
@@ -5,39 +5,40 @@ import 'cards/classica.dart';
 import 'cards/popular.dart';
 import 'cards/totoloto.dart';
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(MaterialApp(title: 'Jogos da Sorte', initialRoute: '/', routes: {
+      '/': (context) => MyApp(),
+    }));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Jogos da Sorte',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Jogos da Sorte'),
-            backgroundColor: Colors.green[700],
-          ),
-          body: new ListView(shrinkWrap: true, children: <Widget>[
-            EuromilhoesCard(
-              number1: '1',
-              number2: '2',
-              number3: '3',
-              number4: '4',
-              number5: '5',
-              star1: '1',
-              star2: '2',
-            ),
-            MilhaoCard(milhaoNumber: '12345'),
-            PopularCard(popularNumber: '12345'),
-            ClassicaCard(classicaNumber: '12345'),
-            TotolotoCard(
-                number1: '1',
-                number2: '2',
-                number3: '3',
-                number4: '4',
-                number5: '5',
-                luckNumber: '6'),
-          ])),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Resultados'),
+        backgroundColor: Colors.green[700],
+      ),
+      body: new ListView(shrinkWrap: true, children: <Widget>[
+        EuromilhoesCard(
+          number1: '1',
+          number2: '2',
+          number3: '3',
+          number4: '4',
+          number5: '5',
+          star1: '1',
+          star2: '2',
+        ),
+        MilhaoCard(milhaoNumber: '12345'),
+        PopularCard(popularNumber: '12345'),
+        ClassicaCard(classicaNumber: '12345'),
+        TotolotoCard(
+            number1: '1',
+            number2: '2',
+            number3: '3',
+            number4: '4',
+            number5: '5',
+            luckNumber: '6'),
+      ]),
     );
   }
 }
